40699cd81f3f26428f6034a6d96e784bd22ad5fd
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-07-07 04:00:14 +0700

    Firebase Deploy

--- src/database.js
+++ src/database.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+//const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNTIwNzI1MiwiZXhwIjoxOTQwNzgzMjUyfQ.PtV3JxOK73xxZ41446OZU_2PWU613WMyrNHeMCJuWJE';
+//const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseUrl = 'https://scneolfmptlydobuvlkh.supabase.co';
 //console.log({ supabaseKey, supabaseUrl });
 
 const supabase = createClient(supabaseUrl, supabaseKey);
