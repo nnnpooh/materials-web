6c68060d5802f4841b28420408603982d55bc8e3
nnnpooh <nnnpooh@gmail.com>
2021-07-12 10:21:24 +0700

    working on class attendance

--- src/components/ClassAttend.js
+++ src/components/ClassAttend.js
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import supabase from '../database';
 
 function ClassAttend({ user }) {
+  const [classCodes, setClassCodes] = useState([]);
+
   useEffect(() => {
     // effect;
 
     async function fetchData() {
-      cpnst { data: checkins_users_details_pivot, error } = await supabase
+      const { data: dataCodes, error: errorCodes } = await supabase
+        .from('codes_pivot')
+        .select('*');
+
+      // if (!error) {
+      //   setClass
+      // }
+
+      const { data: dataCheckins, error: errorCheckins } = await supabase
         .from('checkins_users_details_pivot')
         .select('firstname');
+
+      console.log({ dataCheckins, errorCheckins });
     }
 
     fetchData();
